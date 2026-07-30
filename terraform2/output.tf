4f3136d2ef7bd47574f0d4b14123bf0e68d61bae
fareezio <fareezizzudin.othman@gmail.com>
2026-07-30 23:16:07 +0800

    add 1 more public ec2 and private ec2

--- terraform2/output.tf
+++ terraform2/output.tf
@@ -5,3 +5,19 @@ output "server_ip_1" {
 output "ssm_command_1" {
   value = "aws ssm start-session --target ${aws_instance.my_server_1.id}"
 }
+
+output "server_2_public_ip" {
+  value = aws_instance.my_server_2.public_ip
+}
+
+output "server_private_private_ip" {
+  value = aws_instance.my_private_server.private_ip
+}
+
+output "server_2_ssm" {
+  value = "aws ssm start-session --target ${aws_instance.my_server_2.id}"
+}
+
+output "server_private_ssm" {
+  value = "aws ssm start-session --target ${aws_instance.my_private_server.id}"
+}
