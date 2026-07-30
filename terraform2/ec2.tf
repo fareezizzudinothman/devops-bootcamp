6cdde30b5fde6b90693232ab3b827d9311e85406
fareezio <fareezizzudin.othman@gmail.com>
2026-07-30 22:56:46 +0800

    add other tf file

--- terraform2/ec2.tf
+++ terraform2/ec2.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "my_ami" {
   most_recent = true
-  owners      = ["990723917403"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
@@ -10,4 +10,17 @@ data "aws_ami" "my_ami" {
 
 data "aws_iam_instance_profile" "my_ssm_profile" {
   name = "EC2-SSM-Role"
+}
+
+# < bersambung
+resource "aws_instance" "my_server_1" {
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  tags = {
+    Name = "tf-server-1"
+  }
 }
